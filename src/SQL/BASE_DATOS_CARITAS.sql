462d691b6d4d55410d83c76618c2e29b0467191b
marioxe301 <mariogflores16@gmail.com>
2019-09-04 15:09:53 -0600

    Cambios a SQL de la base de datos

--- src/SQL/BASE_DATOS_CARITAS.sql
+++ src/SQL/BASE_DATOS_CARITAS.sql
@@ -1461,3 +1461,29 @@ INSERT INTO `CARITAS`.`DEPARTAMENTOS` (`id_departamento`, `nombre`) VALUES ('17'
 INSERT INTO `CARITAS`.`DEPARTAMENTOS` (`id_departamento`, `nombre`) VALUES ('18', 'Yoro');
 
 COMMIT;
+
+-- Cambio al view de remision
+USE `CARITAS`;
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `caritas`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `VIEW_REMISION` AS
+    SELECT 
+        `REMISION`.`id_remision` AS `id_remision`,
+        `REMISION`.`juez` AS `juez`
+    FROM
+        `REMISION`;
+-- Update de estados de atencion modificado
+USE `CARITAS`;
+DROP procedure IF EXISTS `UPDATE_ESTADO_ATENCION`;
+
+DELIMITER $$
+USE `CARITAS`$$
+CREATE DEFINER=`caritas`@`localhost` PROCEDURE `UPDATE_ESTADO_ATENCION`(ID INT,ESTADO VARCHAR(45))
+BEGIN
+update ESTADO_ATENCION set estado = ESTADO where id_estadoa = ID;
+END$$
+
+DELIMITER ;
+
--- src/SQL/Cambios.sql
+++ src/SQL/Cambios.sql
@@ -10,4 +10,17 @@ VIEW `VIEW_REMISION` AS
         `REMISION`.`juez` AS `juez`
     FROM
         `REMISION`;
+-- Update de estados de atencion modificado
+USE `CARITAS`;
+DROP procedure IF EXISTS `UPDATE_ESTADO_ATENCION`;
+
+DELIMITER $$
+USE `CARITAS`$$
+CREATE DEFINER=`caritas`@`localhost` PROCEDURE `UPDATE_ESTADO_ATENCION`(ID INT,ESTADO VARCHAR(45))
+BEGIN
+update ESTADO_ATENCION set estado = ESTADO where id_estadoa = ID;
+END$$
+
+DELIMITER ;
+
 
