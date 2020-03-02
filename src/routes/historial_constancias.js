09adf2dba86e7cde427f095f4a36ba3072e2145b
davidxdiaz <davidxdiaz@unitec.edu>
2020-03-02 10:33:57 -0600

    Listo para hacer auditoria de pacientes

--- src/routes/historial_constancias.js
+++ src/routes/historial_constancias.js
@@ -1,26 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mySqlConnection = require('../database');
+const mySqlConnection = require("../database");
 
-
-
-router.get('/api/historial/:ID', (req, res) => {
-    const{ID} = req.params;
-    const query = `
+router.get("/api/historial/:ID", (req, res) => {
+  const { ID } = req.params;
+  const query = `
     select * from historial_constancias where id_paciente = ${ID}
     `;
-    mySqlConnection.query(query,[ID],(error,result,fields)=>{
-        if(!error){
-            if(fields.length !==0){
-                res.send(result);
-            }else{
-                res.send('No hay datos');
-            }
-        }else{
-            res.send('Ocurrio un error al obtener')
-        }
-    });
+  mySqlConnection.query(query, [ID], (error, result, fields) => {
+    if (!error) {
+      if (fields.length !== 0) {
+        res.send(result);
+      } else {
+        res.send("No hay datos");
+      }
+    } else {
+      res.send("Ocurrio un error al obtener");
+    }
+  });
 });
 
+router.get("/api/auditoria/paciente/:ID", (req, res) => {
+  const { ID } = req.params;
+  const query = `
+    select usuario_creacion, usuario_modifico ,fecha_creacion ,fehca_modificacion from paciente where id_paciente = ${ID}
+    `;
+  mySqlConnection.query(query, [ID], (error, result, fields) => {
+    if (!error) {
+      if (fields.length !== 0) {
+        res.send(result);
+      } else {
+        res.send("No hay datos");
+      }
+    } else {
+      res.send("Ocurrio un error al obtener");
+    }
+  });
+});
 
 module.exports = router;
